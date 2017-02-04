3eef90bf64d353df18bbfe090917f879c68238bf
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-02-04 13:56:31 +0100

    testy dla helperów: formatu i konwerterów

---
+++ src/UnitTests/Fixtures/ConvertersFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model.Helpers;
+using Xunit;
+
+namespace UnitTests.Fixtures
+{
+    public class ConvertersFixture
+    {
+        [Fact]
+        public void StringToBytesConvertTest()
+        {
+            var test = "learnourlanguage";
+            var bytes = ConvertersHelper.GetBytes(test);
+
+            Assert.Equal(bytes[0], 108);
+            Assert.Equal(bytes[2], 101);
+            Assert.Equal(bytes[4], 97);
+            Assert.Equal(bytes[6], 114);
+            Assert.Equal(bytes[8], 110);
+            Assert.Equal(bytes[10], 111);
+            Assert.Equal(bytes[12], 117);
+            Assert.Equal(bytes[14], 114);
+            Assert.Equal(bytes[16], 108);
+            Assert.Equal(bytes[18], 97);
+            Assert.Equal(bytes[20], 110);
+            Assert.Equal(bytes[22], 103);
+            Assert.Equal(bytes[24], 117);
+            Assert.Equal(bytes[26], 97);
+            Assert.Equal(bytes[28], 103);
+            Assert.Equal(bytes[30], 101);
+        }
+
+        [Fact]
+        public void BytesToStringConverter()
+        {
+            var expected = "WeAreTheChampions";
+
+            var bytes = new byte[]
+            {
+                87, 0, 101, 0, 65, 0, 114, 0, 101, 0, 84, 0, 104, 0, 101, 0, 67,
+                0, 104, 0, 97, 0, 109, 0, 112, 0, 105, 0, 111, 0, 110, 0, 115, 0
+            };
+            var result = ConvertersHelper.GetString(bytes);
+            Assert.Equal(result, expected);
+        }
+    }
+}
---
+++ src/UnitTests/Fixtures/FormatHelperFixtures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Model.Helpers;
+using Xunit;
+
+namespace UnitTests.Fixtures
+{
+    public class FormatHelperFixtures
+    {
+        [Fact]
+        public void DateFormatValidation()
+        {
+            TestDateFormat(new DateTime(2017, 1, 1), "01.01.2017");
+            TestDateFormat(new DateTime(2017, 12, 1), "01.12.2017");
+            TestDateFormat(new DateTime(2017, 1, 31), "31.01.2017");
+            TestDateFormat(new DateTime(2017, 12, 12), "12.12.2017");
+        }
+
+        [Fact]
+        public void DateTimeFormatValidation()
+        {
+            TestDateTimeFormat(new DateTime(2017, 1, 1, 7, 50, 0), "01.01.2017 07:50");
+            TestDateTimeFormat(new DateTime(2017, 12, 1, 19, 50, 0), "01.12.2017 19:50");
+            TestDateTimeFormat(new DateTime(2017, 1, 31, 23, 59, 59), "31.01.2017 23:59");
+            TestDateTimeFormat(new DateTime(2017, 12, 12, 0, 1, 0), "12.12.2017 00:01");
+        }
+
+        private void TestDateFormat(DateTime date, string excepted)
+        {
+            var data = FormatHelper.DateFormat(date);
+            Assert.Equal(data, excepted);
+        }
+
+        private void TestDateTimeFormat(DateTime datetime, string excepted)
+        {
+            var data = FormatHelper.DateTimeFormat(datetime);
+            Assert.Equal(data, excepted);
+        }
+    }
+}
