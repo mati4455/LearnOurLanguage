e98f2b208ae79d2bfc24791b4fb0bbd7f05bfbd3
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-02-07 22:29:44 +0100

    [Front-end] poprawki kosmetyczne

--- src/LearnOurLanguage.Web/angular2App/app/modules/user/user.routes.ts
+++ src/LearnOurLanguage.Web/angular2App/app/modules/user/user.routes.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DictionariesListComponent } from './components/dictionaries/dictionaries.list.component';
 
 const routes: Routes = [
-    { 
+    {
         path: '',
         component: LayoutComponent,
         children: [
