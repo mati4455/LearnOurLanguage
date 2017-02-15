6970f2b82db5a1e2ca1da3be949324c321cb7ba0
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-02-15 20:28:44 +0100

    obsługa słowników dla użytkownika

--- src/Model/Repositories/DictionariesRepository.cs
+++ src/Model/Repositories/DictionariesRepository.cs
@@ -1,4 +1,7 @@
-﻿using Model.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Model.Base;
 using Model.Models;
 using Model.Models.Database;
 using Model.Repositories.Interfaces;
@@ -10,6 +13,23 @@ namespace Model.Repositories
         public DictionariesRepository(DatabaseContext db) : base(db)
         {
             
+        }
+
+        public IList<Dictionary> GetForUser(int userId)
+        {
+            return GetAll()
+                .Include(x => x.FirstLanguage)
+                .Include(x => x.SecondLanguage)
+                .Where(x => x.UserId == userId)
+                .ToList();
+        }
+        public IList<Dictionary> GetAllPublic()
+        {
+            return GetAll()
+                .Include(x => x.FirstLanguage)
+                .Include(x => x.SecondLanguage)
+                .Where(x => x.Public)
+                .ToList();
         }
     }
 }
--- src/Model/Repositories/Interfaces/IDictionariesRepository.cs
+++ src/Model/Repositories/Interfaces/IDictionariesRepository.cs
@@ -1,9 +1,12 @@
-﻿using Model.Base;
+﻿using System.Collections.Generic;
+using Model.Base;
 using Model.Models.Database;
 
 namespace Model.Repositories.Interfaces
 {
     public interface IDictionariesRepository : IRepositoryBase<Dictionary>
     {
+        IList<Dictionary> GetForUser(int userId);
+        IList<Dictionary> GetAllPublic();
     }
 }
