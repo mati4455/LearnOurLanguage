6edb2ad692b6e92aa89796820dcfe3bce003c0c1
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-02-20 23:22:54 +0100

    import / eksport - formularz

--- src/LearnOurLanguage.Web/angular2App/app/modules/user/components/dictionaries/form/dictionaries.form.component.ts
+++ src/LearnOurLanguage.Web/angular2App/app/modules/user/components/dictionaries/form/dictionaries.form.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import './dictionaries.form.scss';
+let $ = require('jquery');
+
 @Component({
     selector: 'dictionaries-form',
     templateUrl: 'dictionaries.form.component.html',
@@ -18,6 +20,7 @@ export class DictionariesFormComponent {
 
     public dictionary: DictionaryModel = new DictionaryModel();
     public translations: Array<TranslationModel> = [];
+    public isCollapsed: boolean = true;
 
     constructor(
         private translationsService: TranslationsService,
@@ -33,6 +36,7 @@ export class DictionariesFormComponent {
         me.route.params.forEach((params: Params) => {
             let id = +params['dictionaryId'];
             if (userId > 0 && id > 0) {
+                me.isCollapsed = true;
                 me.dictionaryService.get(id, me.loadDictionary, me);
                 me.translationsService.getForDictionary(id, me.loadTranslations, me);
             }
@@ -47,5 +51,27 @@ export class DictionariesFormComponent {
     loadDictionary(data: any) {
         let me = this;
         me.dictionary = data;
+    }
+
+    sendFile(event: any) {
+        let me = this;
+        // todo: do poprawki
+        // let files = event.target.files;
+        // let formData = new FormData();
+        // formData.append("files", formData);
+
+        $('#loader').css('display', 'flex');
+        $('#uploadSpace form').submit();
+
+        // me.dictionaryService.importDictionary(me.dictionary.id, formData, me.finishImport, me);
+    }
+
+    finishImport(data: any) {
+        let me = this;
+    }
+
+    exportDictionary(data: any) {
+        let me = this;
+        window.location.href = '/api/DataExchange/Export/' + me.dictionary.id;
     }
 }
