a169e0e6235f87be1ccc5f69f7e2678aacd0c5f0
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2017-02-23 20:20:44 +0100

    [Front-end] Edycje w formularzu i model do Dictionaries/DictionaryDTO.cs

--- src/LearnOurLanguage.Web/angular2App/app/models/dictionary/index.ts
+++ src/LearnOurLanguage.Web/angular2App/app/models/dictionary/index.ts
@@ -1,3 +1,4 @@
 export { TranslationModel } from './translation.model';
 export { LanguageModel } from './language.model';
 export { DictionaryModel } from './dictionary.model';
+export { DictionaryVoModel} from './dictionaryVo.model';
