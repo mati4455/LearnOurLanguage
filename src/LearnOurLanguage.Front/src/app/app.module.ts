204dbfbafa1440ea4ca5b4ff954306e5924b0c80
Mateusz Pacholec <mateuszpacholec@gmail.com>
2017-03-27 16:40:51 +0200

    tymczasowy powrót do angulara 2

--- src/LearnOurLanguage.Front/src/app/app.module.ts
+++ src/LearnOurLanguage.Front/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -28,7 +27,6 @@ let optionsForNotification: any = {
 @NgModule({
     imports: [
         BrowserModule,
-        BrowserAnimationsModule,
         AppRoutes,
         FormsModule,
         ToastModule.forRoot(),
